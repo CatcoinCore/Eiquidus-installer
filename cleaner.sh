ba16b1b2e7ace2419126f7675e7078f17a761884
Cminor-pools <91484011+Cminor-pools@users.noreply.github.com>
2026-06-03 09:56:45 +1000

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -5,7 +5,7 @@ rm -rf .catcoin
 rm -rf eiquidus
 cd /home/explorer/Eiquidus-installer
 #rm -rf Catcoin-v0.9.3.0
-#clean v2.1.1 needs tests/Checks
+#clean v2.1.1 needs tests/checks
 rm install.log
 sudo reboot
 exit 0
